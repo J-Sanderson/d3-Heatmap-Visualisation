1df3889e6519d179ea85151357609ef8e82af850
J Sanderson <ju.a.sanderson@gmail.com>
2018-05-05 23:40:20 +0100

    add key

--- heat.js
+++ heat.js
@@ -16,23 +16,23 @@ d3.json(
       "Dec"
     ];
     var colours = [
-      "5E4FA2",
-      "3288BD",
-      "66C2A5",
-      "ABDDA4",
-      "E6F598",
-      "FFFFBF",
-      "FEE08B",
-      "FDAE61",
-      "F46D43",
-      "D53E4F",
-      "9E0142"
+      "#5E4FA2",
+      "#3288BD",
+      "#66C2A5",
+      "#ABDDA4",
+      "#E6F598",
+      "#FFFFBF",
+      "#FEE08B",
+      "#FDAE61",
+      "#F46D43",
+      "#D53E4F",
+      "#9E0142"
     ]
 
     var w = 500;
     var h = 1600;
     var margin = {
-      top: 80,
+      top: 85,
       bottom: 30,
       left: 40,
       right: 20
@@ -43,12 +43,15 @@ d3.json(
       .attr("width", w)
       .attr("height", h);
     
+    var minTemp = d3.min(data.monthlyVariance, function(d) {
+          return d.variance + data.baseTemperature;
+        });
+    var maxTemp = d3.max(data.monthlyVariance, function(d) {
+          return d.variance + data.baseTemperature;
+        });
+    
     var colScale = d3.scaleQuantile()
-      .domain([d3.min(data.monthlyVariance, function(d) {
-          return d.variance + data.baseTemperature;
-        }), d3.max(data.monthlyVariance, function(d) {
-          return d.variance + data.baseTemperature;
-        })])
+      .domain([minTemp, maxTemp])
       .range(colours)
 
     var yScale = d3.scaleLinear()
@@ -72,7 +75,77 @@ d3.json(
     var boxWidth = Math.round((w - (margin.left + margin.right)) / 12);
     var boxHeight = Math.round((h - margin.top - margin.bottom) / (data.monthlyVariance.length / 12));
     var keyBox = Math.round((w - (margin.left + margin.right)) / colours.length)
-
+    var keyStep = (maxTemp - minTemp) / colours.length;
+    
+    svg.selectAll(".key.rect")
+      .data(colours)
+      .enter()
+      .append("rect")
+      .attr("y", margin.top - keyBox)
+      .attr("x", function(d, i) {
+        return (margin.left + (i * keyBox));
+      })
+      .attr("width", keyBox)
+      .attr("height", keyBox / 2)
+      .style("fill", function(d) {
+        return d;
+      })
+    
+    svg.selectAll(".key.text")
+      .data(colours)
+      .enter()
+      .append("text")
+      .text(function(d, i){
+        if (colScale.quantiles()[i]) {
+          return colScale.quantiles()[i].toFixed(2);
+        }
+      })
+      .attr("y", (margin.top - keyBox) - 5)
+      .attr("x", function(d, i) {
+        return (margin.left + (i * keyBox)) +(keyBox / 2) + 4;
+      })
+      .attr("font-size", "12px")
+    
+    svg.append("text")
+      .text("Temperature (°C)")
+      .attr("text-anchor", "middle")
+      .attr("y", (margin.top - keyBox) - 22)
+      .attr("x", w / 2)
+      .attr("font-size", "15px")
+    
+    var tip = d3.tip().attr("class", "d3-tip").html(function(d){
+      var label = "<p><strong>" +
+                months[d.month - 1] +
+                " "+
+                d.year +
+                "</strong></p><p>" +
+                (d.variance + data.baseTemperature).toFixed(2) +
+                "&deg;C<br>Variance: " +
+                d.variance +
+                "&deg;C</p>"
+      return label;
+    })
+    svg.call(tip);
+    
+    svg
+      .selectAll(".plot.rect")
+      .data(data.monthlyVariance)
+      .enter()
+      .append("rect")
+      .attr("x", function(d) {
+        return ((d.month * boxWidth) + 3); //?
+      })
+      .attr("y", function(d) {
+        return yScale(d.year);
+      })
+      .attr("height", boxHeight)
+      .attr("width", boxWidth)
+      .style("fill", function(d){
+        return colScale(d.variance + data.baseTemperature);
+      })
+      .on('mouseover', tip.show)
+			.on('mouseout', tip.hide);
+    
     svg
       .append("g")
       .attr("class", "axis")
@@ -84,53 +157,6 @@ d3.json(
       .attr("class", "axis")
       .attr("transform", "translate(0," + (h - margin.bottom) + ")")
       .call(xAxis);
-    
-    svg.selectAll(".key.rect")
-      .data(colours)
-      .enter()
-      .append("rect")
-      .attr("y", margin.top / 4)
-      .attr("x", function(d, i) {
-        return (margin.left + (i * keyBox));
-      })
-      .attr("width", keyBox)
-      .attr("height", keyBox)
-      .style("fill", function(d) {
-        return d;
-      })
-    
-    var tip = d3.tip().attr("class", "d3-tip").html(function(d){
-      var label = "<p><strong>" +
-                months[d.month - 1] +
-                " "+
-                d.year +
-                "</strong></p><p>" +
-                (d.variance + data.baseTemperature).toFixed(2) +
-                "<br>Variance: " +
-                d.variance +
-                "</p>"
-      return label;
-    })
-    svg.call(tip);
-    
-    svg
-      .selectAll(".plot.rect")
-      .data(data.monthlyVariance)
-      .enter()
-      .append("rect")
-      .attr("x", function(d) {
-        return ((d.month * boxWidth) + 3) //?
-      })
-      .attr("y", function(d) {
-        return yScale(d.year);
-      })
-      .attr("height", boxHeight)
-      .attr("width", boxWidth)
-      .style("fill", function(d){
-        return colScale(d.variance + data.baseTemperature)
-      })
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
     
   }
 );
